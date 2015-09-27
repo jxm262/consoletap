0e85e5dcbda5f4a6460d850b407595a406e30e5d
Justin Maat <justin.maat@wendys.com>
2015-09-27 14:40:10 -0400

    -updating manifest -changing styling -adding 3 libs as a default

--- menu.js
+++ menu.js
@@ -1,10 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var url = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min.js';
+var urls = {
+    jquery: 'https://code.jquery.com/jquery-2.1.4.min.js',
+    lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min.js',
+    underscore: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'
+};
 
-    var lib = "var s = document.createElement('script');\
+function menuItemClick(e) {
+    var script = urls[e.target.id];
+
+    var loadScript = "var s = document.createElement('script');\
     s.type = 'text/javascript';\
-    s.src = '" + url + "';\
+    s.src = '" + script + "';\
     (document.getElementsByTagName('head')[0] ||document.getElementsByTagName('body')[0]).appendChild(s);";
 
-    chrome.tabs.executeScript(null,{code:lib});
+    chrome.tabs.executeScript(null,{code:loadScript, allFrames: true});
+
+    window.close();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuItems = document.querySelectorAll('.menu-item');
+    for (var i=0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', menuItemClick);
+    }
 });
